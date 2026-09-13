d43bc3021984a45ca40ef4d3381877a519a5c3cb
EvilMastermod <f16evilmastergpt@gmail.com>
2026-09-13 21:30:13 +0300

    Hide vanilla crosshair when Sakura crosshair is enabled

---
+++ src/main/java/me/sakuravisuals/mixin/GuiMixin.java
@@ -0,0 +1,20 @@
+package me.sakuravisuals.mixin;
+
+import me.sakuravisuals.client.SakuraVisualsClient;
+import net.minecraft.client.DeltaTracker;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiGraphics;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Gui.class)
+public abstract class GuiMixin {
+    @Inject(method = "renderCrosshair", at = @At("HEAD"), cancellable = true)
+    private void sakuraVisuals$hideVanillaCrosshair(GuiGraphics graphics, DeltaTracker deltaTracker, CallbackInfo ci) {
+        if (SakuraVisualsClient.CONFIG.crosshair) {
+            ci.cancel();
+        }
+    }
+}
